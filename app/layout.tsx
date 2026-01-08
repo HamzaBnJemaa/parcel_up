2923c390fe2c197186b205aa003f9c658b364cc7
HamzaBnJemaa <benjemaahamza09@gmail.com>
2026-01-08 22:50:14 +0100

    iconfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,20 +24,7 @@ export const metadata: Metadata = {
     "Interface studies in controlled environments. We design systems that behave, not just screens that display.",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
+    icon: "/parcelup.png",
     apple: "/apple-icon.png",
   },
 }
